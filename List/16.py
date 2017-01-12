7a47a45f528265e949f184adbe288811c38cba5a
monteua <arximed.monte@gmail.com>
2017-01-12 19:10:26 +0200

    "Exercises 16-17"

---
+++ List/16.py
@@ -0,0 +1,8 @@
+'''
+Write a Python program to get the difference between the two lists
+'''
+
+list1 = [1, 4, 5, 3, 1, 5, 6, 5]
+list2 = [2, 5, 4, 9, 7, 3, 5, 4]
+
+print (set(list1) - set(list2))
---
+++ List/17.py
@@ -0,0 +1,8 @@
+'''
+Write a Python program access the index of a list.
+'''
+
+list1 = [1, 4, 5, 3, 1, 5, 6, 5]
+
+for index, value in enumerate(list1):
+    print (index, value)
