fe1b0a898f746ee61f92ebe04d40e6dfbb93c952
monteua <arximed.monte@gmail.com>
2017-01-19 10:45:06 +0200

    New exercises 6-10

---
+++ Dictionary/10.py
@@ -0,0 +1,5 @@
+#Write a Python program to sum all the items in a dictionary.
+
+dict1 = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49}
+
+print(sum(dict1.values()))
---
+++ Dictionary/9.py
@@ -0,0 +1,6 @@
+#Write a Python program to iterate over dictionaries using for loops
+
+dictionary = {5:50,6:60, 'x':"hello", 'y':5}
+print ("Key", "  Value")
+for key, value in dictionary.items():
+    print (key, ' >', "", value)
